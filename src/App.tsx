2a85f5c869c6957d0823a52083609b89fd8e1ced
Michael Caron <Michael.caron@gmail.com>
2020-12-17 19:53:28 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn NRU
         </a>
         {message && <p>{message}</p>}
       </header>
